21a9cc8280b950a4e801174741cb77921c7df430
Ben Atkin <ben@benatkin.com>
2011-01-09 20:55:58 -0700

    add created at time

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -1,10 +1,14 @@
 function(data) {
+  var format_date = function(date) {
+    return new Date(date).toUTCString();
+  };
   // $.log(data)
   var p;
   return {
     items : data.rows.map(function(r) {
       p = (r.value && r.value.profile) || {};
       p.message = r.value && r.value.message;
+      p.created_at = r.value && r.value.created_at && format_date(r.value.created_at);
       return p;
     })
   }
